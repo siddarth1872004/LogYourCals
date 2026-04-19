93c87e490c61d8de97e1f93312e454411fb2660f
siddarth1872004 <sid1872004@gmail.com>
2026-04-19 16:41:37 +0530

    Delete src directory

--- src/features/history.js
+++
@@ -1,8 +0,0 @@
-import { getHistory } from '../core/state.js';
-import { renderHeatmap, renderHistory } from '../ui/history.js';
-
-export function refreshHistoryViews() {
-  const history = getHistory();
-  renderHistory(history);
-  renderHeatmap(history);
-}
--- src/utils/dom.js
+++
@@ -1,9 +0,0 @@
-export const g = (id) => document.getElementById(id);
-
-export function escHtml(value) {
-  return String(value ?? '')
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
-}
